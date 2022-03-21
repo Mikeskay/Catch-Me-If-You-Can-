7c7c59445216b5da4608a74017bae9a722a0e404
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 18:19:28 +0000

    Create CatchMeTest.java

---
+++ test/CatchMeTest.java
@@ -0,0 +1,17 @@
+package catchme.test;
+
+import catchme.base.CatchMe;
+
+public class CatchMeTest {
+
+    public static void main(String[] args) {
+
+        
+        //System.out.println(panels.getName());
+        CatchMe test = new CatchMe();
+           
+            
+        }
+
+
+}
